04b1448b4bda4f0c226b77df75c3a2ec3bb7a80c
raul <raulramirez@opendeusto.es>
2018-01-26 17:28:42 +0100

    Unión de principal y pantalla principal, la manzana aparece siempre. No hay errores. Todo funciona correctamente.

--- src/codigo/GestorVentanas.java
+++ src/codigo/GestorVentanas.java
@@ -9,17 +9,17 @@ import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.FocusAdapter;
-import java.awt.event.FocusEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.io.LineNumberInputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Random;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -27,24 +27,20 @@ import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
-import javax.swing.text.SimpleAttributeSet;
-import javax.swing.text.StyleConstants;
-import javax.swing.text.StyledDocument;
 
 import COMUN.clsUsuarioVacio;
 import codigo.GameOver.Estadisticas;
-import codigo.PantallaPrincipal.MiRunnable;
 
 
 
 public class GestorVentanas 
 {
-	
-	private static ArrayList<JFrame> listaVentanas = init();
 
+	public static ArrayList<JFrame> listaVentanas = init();
+	
+	
 	
 	private static ArrayList<JFrame> init() {
 		ArrayList<JFrame> lista = new ArrayList<JFrame>();
@@ -64,7 +60,6 @@ public class GestorVentanas
 		}
 		listaVentanas.clear();
 	}
-	
 	
 	public static void hacerVisible(Class<?> ventanaAVisibilizar, boolean ocultarElResto, int numDeVentana ) {
 		for (JFrame vent : listaVentanas) {
@@ -102,78 +97,38 @@ public class GestorVentanas
 		}
 	}
 	
-	public static void cerrar ()
-	{
-		Principal Ventana1;
-		PantallaPrincipal Ventana2;
-		Ventana1 = (Principal) listaVentanas.get(2);
-		Ventana2 = (PantallaPrincipal) listaVentanas.get(3);
+	
+	public static  void cerrar ()
+	{		
+		Principal Ventana1 = (Principal) listaVentanas.get(1);
 		
-		Ventana2.miHilo.acaba();
-		Ventana2.miHilo2.acaba();
-		Ventana2.miHilo3.acaba();
+		Ventana1.miHilo.acaba();
+		Ventana1.miHilo2.acaba();
+		Ventana1.miHilo3.acaba();
 		
-		Ventana2.dispose();
+		Ventana1.growUp = 1;
+		Ventana1.creaBloque();
+		Ventana1.miManzana = null;
+		
+		Ventana1.pPrincipal.removeAll();
+		Ventana1.pPrincipal.repaint();
+		
 		Ventana1.dispose();
 		
-		listaVentanas.remove(3);
-		listaVentanas.remove(2);
-		
-		
-		Principal.pPrincipal.removeAll();
-		Principal.pPrincipal.repaint();
-		
-		Principal.growUp = 1;
-		Principal.creaBloque();
-		Principal.miManzana = null;
-		
-		
-		
+		listaVentanas.remove(1);
 	}
 
 	
 	
 	public static void main (String s[]) {
-		add( new PantallaInicio() );
-	
-		add( new GameOver() );
 		String es = "estadisticas";
 		clsBD.initBD(es);
 		clsBD.crearTablaEstadisticas();
 		
-		try
-		{
+		add( new PantallaInicio() );
+		add( new GameOver() );
 		
 		hacerVisible( PantallaInicio.class, true, 0 ); 
-		if(PantallaInicio.jugar.isSelected()==true)
-		{
-		hacerVisible( Principal.class, true, 0 ); 
-		}
-		else
-		{
-	
-		}
-
-		if(GameOver.retry.isSelected()==true)
-		{
-		hacerVisible( Principal.class, true, 0 );
-		}
-
-		else if(GameOver.estadisticas.isSelected()==true)
-		{
-			hacerVisible( Estadisticas.class, true, 0);
-		}
-		else
-		{
-		Thread.sleep( 150000 );
-		ocultar( GameOver.class, 0 );
-		}
-		
-		closeAndDispose();
-		}catch(InterruptedException e)
-		{
-			
-		}
 	}
 }
 
@@ -227,28 +182,30 @@ class PantallaInicio extends JFrame
 					
 					if (!Nombre.isEmpty())
 					{	
+						PantallaInicio inicio = (PantallaInicio) GestorVentanas.listaVentanas.get(0);
+						inicio.dispose();
+						GestorVentanas.listaVentanas.remove(0);
+						
 						Principal miVentana = new Principal();
 						GestorVentanas.add(miVentana);
-						PantallaPrincipal miVentana1 = new PantallaPrincipal();
-						GestorVentanas.add(miVentana1);
 					
 						
 						miVentana.creaBloque();
 						miVentana.setVisible( true );
 					
-						miVentana1.miHilo = miVentana1.new MiRunnable(); 
+						miVentana.miHilo = miVentana.new MiRunnable(); 
 					
-						Thread nuevoHilo = new Thread( miVentana1.miHilo );
+						Thread nuevoHilo = new Thread( miVentana.miHilo );
 						nuevoHilo.start();
 					
-						miVentana1.miHilo2 = miVentana1.new RandomApple();
+						miVentana.miHilo2 = miVentana.new RandomApple();
 					
-						Thread nuevoHilo2 = new Thread( miVentana1.miHilo2 );
+						Thread nuevoHilo2 = new Thread( miVentana.miHilo2 );
 						nuevoHilo2.start();
+						
+						miVentana.miHilo3 = miVentana.new cronometro();
 					
-						miVentana1.miHilo3 = miVentana1.new cronometro();
-					
-						Thread nuevoHilo3 = new Thread (miVentana1.miHilo3 );
+						Thread nuevoHilo3 = new Thread (miVentana.miHilo3 );
 						nuevoHilo3.start();
 				
 					} else  
@@ -279,15 +236,21 @@ class PantallaInicio extends JFrame
 class Principal extends JFrame
 {
 	private static final long serialVersionUID = 1L; 
-	static JPanel pPrincipal; 
+	 JPanel pPrincipal; 
 	JPanel pCabecera;
-	static bloqueJuego [] miBloque; 
-	static manzana miManzana = null;
-	PantallaPrincipal.MiRunnable miHilo = null;
-	PantallaPrincipal.RandomApple miHilo2 = null;
-	PantallaPrincipal.cronometro miHilo3 = null;
-	boolean posible = true;
-	static int growUp = 1;
+	 bloqueJuego [] miBloque; 
+	 manzana miManzana = null;
+	 int growUp = 1;
+	 
+	 MiRunnable miHilo = null;
+		RandomApple miHilo2 = null;
+		cronometro miHilo3 = null;
+		boolean posible = true;
+		int puntuacion = 0;
+		String tiempofinal;
+		
+		public static int Altura;
+		public static int Anchura;
 	
 	public Principal()
 	{
@@ -308,7 +271,11 @@ class Principal extends JFrame
 			add( pCabecera, BorderLayout.NORTH );
 			
 			
-			setSize( 850, 750);
+			setSize( 800, 800);
+			Altura = 800 - 150;
+			Anchura = 800 - 150;
+			
+			this.setResizable(false);
 			
 			this.addKeyListener( new KeyAdapter() {
 				@Override
@@ -394,7 +361,7 @@ class Principal extends JFrame
 			});
 		
 	}
-	public static void creaBloque( ) 
+	public void creaBloque( ) 
 	{
 
 		miBloque = new bloqueJuego[300];
@@ -405,7 +372,316 @@ class Principal extends JFrame
 
 	}
 	
+	class MiRunnable implements Runnable {
+		boolean sigo = true;
+		String Nombre = PantallaInicio.Nombre;
+		private Logger logger = Logger.getLogger(MiRunnable.class.getName());
+		Player a;
+
+		@Override
+		public void run() {
+			logger.setLevel(Level.INFO);
+			
+			while (sigo) {
+				
+			posible = false;
+			
+			Principal miVentana = (Principal) GestorVentanas.listaVentanas.get(1);
+
+			double xAnterior = miVentana.miBloque[0].getPosX();
+			double yAnterior = miVentana.miBloque[0].getPosY();
+			
+			miVentana.miBloque[0].mueveX();
+			miVentana.miBloque[0].mueveY();
+			
+			for (int i = 1; i<miVentana.growUp; i++)
+			{
+				double xGuardado = miVentana.miBloque[i].getPosX();
+				double yGuardado = miVentana.miBloque[i].getPosY();
+				
+				miVentana.miBloque[i].setPosicion(xAnterior, yAnterior);
+				xAnterior = xGuardado;
+				yAnterior = yGuardado;
+			}
+				
+				
+			
+			posible = true;
+				
+				if (miVentana.miBloque[0].getPosX() < -JLabelBloque.TAMANYO_BLOQUE/2 || miVentana.miBloque[0].getPosX()>miVentana.pPrincipal.getWidth()-JLabelBloque.TAMANYO_BLOQUE/2 ) {
+					
+					a = RegistrarJugador(Nombre, puntuacion, tiempofinal);
+					Date fechayHora = a.getFecha();
+					SimpleDateFormat formato = new SimpleDateFormat( "dd/MM/yyyy HH:mm:ss" );						
+					
+					String fecha = formato.format(fechayHora);
+					logger.info("El juego ha acabado");
+					
+					
+					clsBD.InsertEstadisticas(a.getNick(), a.getPuntuacion(), a.getTiempo(), fecha);
+					
+					logger.info("El usuario se ha registrado");
+
+					
+					logger.info("La puntuación es de: " + puntuacion);
+					
+					GestorVentanas.hacerVisible( GameOver.class, true, 0);
+					
+					miHilo2.acaba();
+					miHilo3.acaba();
+					this.acaba();
+					
+				}
+				
+				if (miVentana.miBloque[0].getPosY() < -JLabelBloque.TAMANYO_BLOQUE/2 || miVentana.miBloque[0].getPosY()>miVentana.pPrincipal.getHeight()-JLabelBloque.TAMANYO_BLOQUE/2 ) {
+				
+					a = RegistrarJugador(Nombre, puntuacion, tiempofinal);
+					Date fechayHora = a.getFecha();
+					SimpleDateFormat formato = new SimpleDateFormat( "dd/MM/yyyy HH:mm:ss" );						
+					
+					String fecha = formato.format(fechayHora);
+					logger.info("El juego ha acabado");
+				
+					
+					clsBD.InsertEstadisticas(a.getNick(), a.getPuntuacion(), a.getTiempo(), fecha);
+					
+					logger.info("El usuario se ha registrado");
+
+					
+					logger.info("La puntuación es de: " + puntuacion);
+					
+					GestorVentanas.hacerVisible( GameOver.class, true, 0);
+					
+					miHilo2.acaba();
+					miHilo3.acaba();
+					this.acaba();
+					
+					
+				}
+				
+				for (int i = 1; i < miVentana.growUp;i++)
+				{
+					if (miVentana.miBloque[0].getPosX()==miVentana.miBloque[i].getPosX()&&miVentana.miBloque[0].getPosY()==miVentana.miBloque[i].getPosY())
+					{
+						
+						a = RegistrarJugador(Nombre, puntuacion, tiempofinal);
+						Date fechayHora = a.getFecha();
+						SimpleDateFormat formato = new SimpleDateFormat( "dd/MM/yyyy HH:mm:ss" );						
+						
+						String fecha = formato.format(fechayHora);
+						logger.info("El juego ha acabado");
+						
+						clsBD.InsertEstadisticas(a.getNick(), a.getPuntuacion(), a.getTiempo(), fecha);
+
+						logger.info("El usuario se ha registrado");
+
+						
+						logger.info("La puntuación es de: " + puntuacion);
+						
+						GestorVentanas.hacerVisible( GameOver.class, true, 0);
+						
+						miHilo2.acaba();
+						miHilo3.acaba();
+						this.acaba();
+					}
+				}
+				
+				try {
+					Thread.sleep(200);
+				} catch (Exception e) {
+				}
+			}
+		}
+		
+		public Player RegistrarJugador(String nick, int puntuacion, String tiempo) 
+		{
+			Player a;
+			
+			return a = new Player (nick, puntuacion, tiempo);	
+			
+		}
+
+		
+		public void acaba() 
+		{
+			sigo = false;
+		}
+	};
+	public void growUp()
+	{
+		Principal miVentana = (Principal) GestorVentanas.listaVentanas.get(1);
+		miVentana.miBloque[miVentana.growUp]=new bloqueJuego();
+		miVentana.miBloque[miVentana.growUp].setPosicion(miVentana.miBloque[miVentana.growUp-1].getPosX()-30, miVentana.miBloque[miVentana.growUp-1].getPosY());
+		miVentana.pPrincipal.add(miVentana.miBloque[miVentana.growUp].getGrafico());
+		miVentana.miBloque[miVentana.growUp].setDireccionActual(miVentana.miBloque[miVentana.growUp-1].miDireccionActual);
+		miVentana.growUp++;
+	}
 	
+	class RandomApple implements Runnable {
+		boolean sigo2 = true;
+		@Override
+		public void run() {
+			
+			Random r;
+			int  apple_posX;
+			int  apple_posY;
+			Point P;
+			
+			Principal miVentana = (Principal) GestorVentanas.listaVentanas.get(1);
+			
+			while (true) 
+			{
+				if(miVentana.miManzana == null)
+				{
+					P = RecursividadManzana(5);
+					
+					miVentana.miManzana = new manzana(P.x,P.y);
+					miVentana.pPrincipal.add(miVentana.miManzana);
+					miVentana.pPrincipal.repaint();
+				}
+				
+				
+				if(miVentana.miManzana.getLocation().distance(miVentana.miBloque[0].getPosX(), miVentana.miBloque[0].getPosY()) < 30)
+				{	
+					P = RecursividadManzana(5);
+					
+					miVentana.miManzana.setLocation(P);
+//					miVentana.pPrincipal.add(miVentana.miManzana);
+					miVentana.pPrincipal.repaint();
+					growUp();
+					puntuacion = puntuacion + 1;
+				}
+			
+				
+				
+				try 
+				{	
+				Thread.sleep( 33 );
+				} catch (Exception e) {
+				}
+				
+			}
+			
+		}
+		
+		public Point RecursividadManzana(int NumRecu)
+		{
+			Principal miVentana = (Principal) GestorVentanas.listaVentanas.get(1);
+			Point P = null;
+			Random r;
+			int  apple_posX = 0;
+			int  apple_posY = 0;
+			
+			r= new Random();
+			apple_posX = r.nextInt(Anchura);
+			apple_posY = r.nextInt(Altura);
+			
+			apple_posX = apple_posX + 70;
+			apple_posY = apple_posY + 70;
+			
+			if(NumRecu != 0)
+			{	
+				for(int i = 0; i<miVentana.growUp; i++)
+				{
+					if(new Point(apple_posX, apple_posY).distance(miVentana.miBloque[i].getPosX(), miVentana.miBloque[i].getPosY()) < 40)
+					{
+						RecursividadManzana(NumRecu-1);
+					}else{
+						P = new Point(apple_posX, apple_posY);
+						System.out.println(P);
+						return P;
+					}
+				}
+				
+			}else{
+				P = new Point (200,300);
+				System.out.println(P);
+			}
+		
+		
+			
+			return P;
+		}
+		
+		
+		
+		public void acaba() 
+		{
+			sigo2 = false;
+		}
+		
+	};
+	class cronometro implements Runnable
+	{
+		boolean sigo3 = true;
+		private Logger logger = Logger.getLogger( MiRunnable.class.getName());
+		
+		
+		@Override
+		public void run()
+		{
+			// TODO Auto-generated method stub
+			int segundos = 0;
+			int decimas = 0;
+			
+			Principal miVentana = (Principal) GestorVentanas.listaVentanas.get(1);
+			
+
+			try {
+			while (sigo3)
+				{
+				
+				if(decimas != 9)
+				{ 
+					decimas ++;
+				}
+				else
+				{
+					segundos ++;
+					decimas = 0;
+				
+				}
+				
+					tiempofinal = segundos +"."+decimas;
+					
+					
+					if (miVentana.miBloque[0].getPosX() < -JLabelBloque.TAMANYO_BLOQUE/2 || miVentana.miBloque[0].getPosX()>miVentana.pPrincipal.getWidth()-JLabelBloque.TAMANYO_BLOQUE/2 ) {
+						
+						
+						sigo3 = false;
+						miHilo3.acaba();
+					
+					}
+					
+					else if (miVentana.miBloque[0].getPosY() < -JLabelBloque.TAMANYO_BLOQUE/2 || miVentana.miBloque[0].getPosY()>miVentana.pPrincipal.getHeight()-JLabelBloque.TAMANYO_BLOQUE/2 ) {
+						
+						
+						sigo3 = false;
+						 
+						
+
+						miHilo3.acaba();
+					}
+					
+					
+	         Thread.sleep(99);
+	         }   
+			
+			logger.info("El tiempo es de: " + tiempofinal);
+			
+			
+			
+	     } catch (Exception ex) 
+		{
+	          System.out.println(ex.getMessage());
+	     }                 	
+		}
+		public void acaba() {
+			sigo3 = false;
+		}
+		
+	
+	};
 }
 
 
@@ -456,26 +732,24 @@ JPanel panel;
 				
 				Principal miVentana = new Principal();
 				GestorVentanas.add(miVentana);
-				PantallaPrincipal miVentana1 = new PantallaPrincipal();
-				GestorVentanas.add(miVentana1);
 			
 				
 				miVentana.creaBloque();
 				miVentana.setVisible( true );
 			
-				miVentana1.miHilo = miVentana1.new MiRunnable();  
+				miVentana.miHilo = miVentana.new MiRunnable();  
 			
-				Thread nuevoHilo = new Thread( miVentana1.miHilo );
+				Thread nuevoHilo = new Thread( miVentana.miHilo );
 				nuevoHilo.start();
 			
-				miVentana1.miHilo2 = miVentana1.new RandomApple();
+				miVentana.miHilo2 = miVentana.new RandomApple();
 			
-				Thread nuevoHilo2 = new Thread( miVentana1.miHilo2 );
+				Thread nuevoHilo2 = new Thread( miVentana.miHilo2 );
 				nuevoHilo2.start();
 			
-				miVentana1.miHilo3 = miVentana1.new cronometro();
+				miVentana.miHilo3 = miVentana.new cronometro();
 			
-				Thread nuevoHilo3 = new Thread (miVentana1.miHilo3 );
+				Thread nuevoHilo3 = new Thread (miVentana.miHilo3 );
 				nuevoHilo3.start();
 				
 				
@@ -580,6 +854,7 @@ JPanel panel;
 		
 		public Estadisticas ()
 		{
+			
 			setTitle ("Estadisticas");
 			setSize (800, 600);
 			
@@ -600,20 +875,15 @@ JPanel panel;
 			PanelTexto.setText(players);
 			Cerrar = new JButton ("Cerrar");
 			
-			StyledDocument doc = PanelTexto.getStyledDocument();
-			SimpleAttributeSet center = new SimpleAttributeSet();
-			StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
-			doc.setParagraphAttributes(0, doc.getLength(), center, false);
-			
 			PanelTexto.setVisible(true);
 			PanelTexto.setEditable(false);
 			
 			Scroll.setPreferredSize(new Dimension(700,500));
 			
 			Scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-
 		}
 
 	}
+
 }
 
